657c2a65a0cd8ca74db5815f2fea771bdc5b2b76
Francisco <segovfrank@gmail.com>
2020-06-07 15:14:23 -0500

    Added bot controller to communication

--- communication-service/routes/web.js
+++ communication-service/routes/web.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const paymentController = require('../controllers/payment');
 const statusController = require('../controllers/status');
+const botController = require('../controllers/bot');
 
 router.get('/payment', paymentController.createPayment);
 router.get('/transaction', paymentController.processTransaction);
 router.get('/status/get', statusController.get);
 router.get('/status/update', statusController.update);
+router.post('/receive', botController.receive);
+router.get('/send', botController.send);
 
 module.exports = router;
